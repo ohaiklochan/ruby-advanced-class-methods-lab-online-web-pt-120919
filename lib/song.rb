f7600712614cfd8ee1bedcc2c224b2f2f41a0efa
Chloe Foronda <caforonda103@yahoo.com>
2020-01-22 00:44:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -53,8 +53,8 @@ class Song
   def self.create_from_filename(filename)
     x = self.find_by_name(filename)
     song = self.create
-    song.name = name
-    song.artist_name = artist_name
+    song.name = array[1]
+    song.artist_name = array[0]
     song
   end
 
